4a5de5d95c6b23d3a0b8e51d207a50209465a9f7
Pratik Pangeni <pratikpangeni7283444259@gmail.com>
2026-06-18 16:17:37 +0545

    NFT set

--- src/abi.js
+++ src/abi.js
@@ -1,86 +1,89 @@
-// abi.js
+// abi.js - FOR YOUR ACTUAL CONTRACT
 export const contractABI = [
   {
-    type: "function",
-    name: "addExpense",
-    inputs: [
-      { name: "expname", type: "string", internalType: "string" },
-      { name: "paidby", type: "string", internalType: "string" },
-      { name: "person1", type: "string", internalType: "string" },
-      { name: "person2", type: "string", internalType: "string" },
-      { name: "paddress", type: "string", internalType: "string" },
-      { name: "amt", type: "uint256", internalType: "uint256" },
-      { name: "status", type: "uint8", internalType: "uint8" },
+    "type": "function",
+    "name": "addExpense",
+    "inputs": [
+      { "name": "_description", "type": "string" },
+      { "name": "_payer", "type": "string" },
+      { "name": "_payee", "type": "string" },
+      { "name": "_participant", "type": "string" },
+      { "name": "_place", "type": "string" },
+      { "name": "_amount", "type": "uint256" },
+      { "name": "_status", "type": "uint8" }
     ],
-    outputs: [],
-    stateMutability: "nonpayable",
+    "outputs": [],
+    "stateMutability": "nonpayable"
   },
   {
-    type: "function",
-    name: "expenses",
-    inputs: [{ name: "", type: "uint256", internalType: "uint256" }],
-    outputs: [
-      { name: "expname", type: "string", internalType: "string" },
-      { name: "paidby", type: "string", internalType: "string" },
-      { name: "person1", type: "string", internalType: "string" },
-      { name: "person2", type: "string", internalType: "string" },
-      { name: "paddress", type: "string", internalType: "string" },
-      { name: "amt", type: "uint256", internalType: "uint256" },
-      { name: "shareamount", type: "uint256", internalType: "uint256" },
-      { name: "status", type: "uint8", internalType: "uint8" },
-    ],
-    stateMutability: "view",
-  },
-  {
-    type: "function",
-    name: "getExpense",
-    inputs: [{ name: "index", type: "uint256", internalType: "uint256" }],
-    outputs: [
+    "type": "function",
+    "name": "getExpense",
+    "inputs": [{ "name": "_id", "type": "uint256" }],
+    "outputs": [
       {
-        name: "",
-        type: "tuple",
-        internalType: "struct ExpenseSharing.Expense",
-        components: [
-          { name: "expname", type: "string", internalType: "string" },
-          { name: "paidby", type: "string", internalType: "string" },
-          { name: "person1", type: "string", internalType: "string" },
-          { name: "person2", type: "string", internalType: "string" },
-          { name: "paddress", type: "string", internalType: "string" },
-          { name: "amt", type: "uint256", internalType: "uint256" },
-          { name: "shareamount", type: "uint256", internalType: "uint256" },
-          { name: "status", type: "uint8", internalType: "uint8" },
-        ],
-      },
+        "name": "",
+        "type": "tuple",
+        "components": [
+          { "name": "description", "type": "string" },
+          { "name": "payer", "type": "string" },
+          { "name": "payee", "type": "string" },
+          { "name": "participant", "type": "string" },
+          { "name": "place", "type": "string" },
+          { "name": "amount", "type": "uint256" },
+          { "name": "status", "type": "uint8" }
+        ]
+      }
     ],
-    stateMutability: "view",
+    "stateMutability": "view"
   },
   {
-    type: "function",
-    name: "getLength",
-    inputs: [],
-    outputs: [{ name: "", type: "uint256", internalType: "uint256" }],
-    stateMutability: "view",
+    "type": "function",
+    "name": "getLength",
+    "inputs": [],
+    "outputs": [{ "name": "", "type": "uint256" }],
+    "stateMutability": "view"
   },
   {
-    type: "function",
-    name: "resetexp",
-    inputs: [],
-    outputs: [],
-    stateMutability: "nonpayable",
+    "type": "function",
+    "name": "getStatus",
+    "inputs": [],
+    "outputs": [{ "name": "", "type": "string" }],
+    "stateMutability": "view"
   },
   {
-    type: "function",
-    name: "updateexpense",
-    inputs: [
-      { name: "expname", type: "string", internalType: "string" },
-      { name: "paidby", type: "string", internalType: "string" },
-      { name: "person1", type: "string", internalType: "string" },
-      { name: "person2", type: "string", internalType: "string" },
-      { name: "paddress", type: "string", internalType: "string" },
-      { name: "amt", type: "uint256", internalType: "uint256" },
-      { name: "status", type: "uint8", internalType: "uint8" },
+    "type": "function",
+    "name": "getShareAmount",
+    "inputs": [],
+    "outputs": [{ "name": "", "type": "uint256" }],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "updateexpense",
+    "inputs": [
+      { "name": "_description", "type": "string" },
+      { "name": "_payer", "type": "string" },
+      { "name": "_payee", "type": "string" },
+      { "name": "_participant", "type": "string" },
+      { "name": "_place", "type": "string" },
+      { "name": "_amount", "type": "uint256" },
+      { "name": "_status", "type": "uint8" }
     ],
-    outputs: [],
-    stateMutability: "nonpayable",
+    "outputs": [],
+    "stateMutability": "nonpayable"
   },
+  {
+    "type": "function",
+    "name": "updateStatus",
+    "inputs": [{ "name": "_newStatus", "type": "uint8" }],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "resetexp",
+    "inputs": [],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  }
 ];
